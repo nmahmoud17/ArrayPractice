cb2ee5617489eb1b7675ceda466bb08525976232
Nada Mahmoud <nadamahmoud@Nadas-MacBook-Pro.local>
2019-01-23 15:18:19 -0500

    Converting F to C

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,32 +10,42 @@ public class Main {
 //    Each time the user inputs a value, add it to your dailyAverageTemp array
 //    Next, your program should print out the weekly average temperature
 //    (e.g., the average of the averages)
+//Your program should convert each daily average temperature to Celsius,
+// and should store the Celsius values in a new array named dailyAverageTempCelsius
+//    Your program should print out the daily and weekly averages in Fahrenheit and Celsius
 
     public static void main(String[] args) {
-        int averageTempArraySize = 7;
+        int averageTempFArraySize = 7;
+        int averageTempCArraySize =7;
 
         Scanner reader = new Scanner(System.in);
 
-        double [] dailyAverageTemp = new double [averageTempArraySize];
+        double [] dailyAverageTempF = new double [averageTempFArraySize];
+        double [] dailyAverageTempC = new double [averageTempCArraySize];
 
 
-        for (int i=0; i< averageTempArraySize; i++){
+        for (int i=0; i< averageTempFArraySize; i++){
             System.out.println("Please enter the daily average temperature " +
                     "(in degrees Fahrenheit) for the last 7 days  ");
             double userInput = reader.nextDouble();
-            dailyAverageTemp[i]=userInput;
+            dailyAverageTempF[i]=userInput;
 
         }
 
         double input = 7;
         double sum = 0;
         double weeklyAverage=0;
+        double tempFtoC=0;
 
-
-        for (int i=0; i<dailyAverageTemp.length; i++){
-            System.out.println(dailyAverageTemp [i]);
-            sum = sum + dailyAverageTemp [i];
+        for (int i=0; i<dailyAverageTempF.length; i++){
+            System.out.println(dailyAverageTempF [i]);
+            sum = sum + dailyAverageTempF [i];
             weeklyAverage = sum/input;
+        }
+
+        for (double j: dailyAverageTempF) {
+             tempFtoC = (j-32)/ 1.8;
+             System.out.println("The temperature from F to C is " + tempFtoC);
         }
 
         System.out.println ("The sum is " + sum);
